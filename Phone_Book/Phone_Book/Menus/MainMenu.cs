e9a92b2e8a08d6d9e06f4084c6cf818fcfce6a2c
DSills735 <David.sills100@gmail.com>
2026-03-23 19:32:10 -0500

    test

---
+++ Phone_Book/Phone_Book/Menus/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿
+using Phone_Book.Services;
+using Spectre.Console;
+
+namespace Phone_Book.Menus
+{
+    internal class MainMenu
+    {
+        
+        public static void HomeScreen()
+        {
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Welcome to the Phone book. Please use the arrow keys to select an option.")
+                .AddChoices(new[]
+                {
+                    "Add a contact",
+                    "Update an existing contact",
+                    "Delete a contact",
+                    "View a single contact card",
+                    "View all contacts",
+                    "View contacts by relationship",
+                    "Exit"
+                }));
+
+            switch (choice) 
+            {
+                case "Add a contact":
+                    Controllers.ContactController.AddContact();
+                    break;
+
+                case "Update an existing contact":
+                    Services.ContactService.UpdateContact();
+                    break;
+
+                case "Delete a contact":
+                    ContactService.DeleteContact();
+                    break;
+                case "View a single contact card":
+                    var contact = Services.ContactService.GetContactInputList();
+                    UserInterface.ShowContactCard(contact);
+                    break;
+                case "View contacts by relationship":
+                    Services.ContactService.GetRelationshipInputList();
+                    break;
+
+                case "View all contacts":
+                    Controllers.ContactController.ViewContacts(true);
+                    break;
+
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+              
+            }
+
+        }
+    }
+}
