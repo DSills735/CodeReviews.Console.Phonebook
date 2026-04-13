9ef0400c2446e8fc8379df57ee6d30b621cc906f
DSills735 <David.sills100@gmail.com>
2026-04-13 08:41:05 -0500

    added mnigration to reflect changes.

--- PhoneBook.dsills735/Phone_Book/Migrations/20260413134046_dbupdate.Designer.cs
+++ PhoneBook.dsills735/Phone_Book/Migrations/20260413134046_dbupdate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Phone_Book.Context;
 
@@ -10,9 +11,11 @@ using Phone_Book.Context;
 namespace Phone_Book.Migrations
 {
     [DbContext(typeof(ContactContext))]
-    partial class ContactContextModelSnapshot : ModelSnapshot
+    [Migration("20260413134046_dbupdate")]
+    partial class dbupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PhoneBook.dsills735/Phone_Book/Migrations/20260413134046_dbupdate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Phone_Book.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_contacts",
+                table: "contacts");
+
+            migrationBuilder.RenameTable(
+                name: "contacts",
+                newName: "Contacts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts",
+                column: "ContactID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts");
+
+            migrationBuilder.RenameTable(
+                name: "Contacts",
+                newName: "contacts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_contacts",
+                table: "contacts",
+                column: "ContactID");
+        }
+    }
+}
